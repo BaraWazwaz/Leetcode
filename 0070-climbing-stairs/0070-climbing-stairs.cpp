6040e189dafe64e708a97f8eeb0e5d9789544691
Bara Wazwaz <145051341+BaraWazwaz@users.noreply.github.com>
2026-03-10 14:48:38 +0200

    Time: 0 ms (100%), Space: 8.6 MB (34.3%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,10 +1,36 @@
 class Solution {
+    using Row = vector<long long>;
+    using Matrix = vector<vector<long long>>;
+    Matrix mul(Matrix a, Matrix b) {
+        return {
+            {
+                a[0][0] * b[0][0] + a[0][1] * b[1][0],
+                a[0][0] * b[0][1] + a[0][1] * b[1][1]
+            }, {
+                a[1][0] * b[0][0] + a[1][1] * b[1][0],
+                a[1][0] * b[0][1] + a[1][1] * b[1][1]
+            }
+        };
+    }
+    Matrix power(Matrix a, int p) {
+        Matrix ans = {
+            { 1, 0 },
+            { 0, 1 }
+        };
+        while (p) {
+            if (p & 1)
+                ans = mul(ans, a);
+            a = mul(a, a);
+            p >>= 1;
+        }
+        return ans;
+    }
 public:
     int climbStairs(int n) {
-        auto dp = vector(n + 1, 0);
-        dp[0] = dp[1] = 1;
-        for (int i = 2; i <= n; ++i)
-            dp[i] = dp[i - 1] + dp[i - 2];
-        return dp[n];
+        Matrix a = {
+            { 1, 1 },
+            { 1, 0 }
+        };
+        return power(a, n + 1)[1][0];
     }
 };
