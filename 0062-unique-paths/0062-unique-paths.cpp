0db92737a79941c319d42195aef4adb90307d08c
Bara Wazwaz <145051341+BaraWazwaz@users.noreply.github.com>
2026-03-10 14:40:43 +0200

    Time: 0 ms (100%), Space: 7.9 MB (92.76%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        auto dp = vector(m, vector(n, 0));
-        for (int i = 0; i < n; ++i)
-            dp[0][i] = 1;
+        --n, --m;
+        if (n < m)
+            swap(n, m);
+        n += m;
+        long long ans = 1;
         for (int i = 0; i < m; ++i)
-            dp[i][0] = 1;
-        for (int i = 1; i < m; ++i)
-            for (int j = 1; j < n; ++j)
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-        return dp[m - 1][n - 1];
+            ans = ans * (n - i) / (i + 1);
+        return ans;
     }
 };
