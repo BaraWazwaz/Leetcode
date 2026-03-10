b66c80ff2b47a56e9b73b77fefaecc6bfcefd27a
Bara Wazwaz <145051341+BaraWazwaz@users.noreply.github.com>
2026-03-10 15:02:58 +0200

    Time: 0 ms (100%), Space: 8 MB (47.91%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        auto dp = vector(max(3, n + 1), 0);
-        dp[1] = dp[2] = 1;
+        vector<int> dp = { 0, 1, 1 };
         for (int i = 3; i <= n; ++i)
-            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-        return dp[n];
+            dp[i % 3] = dp[0] + dp[1] + dp[2];
+        return dp[n % 3];
     }
 };
